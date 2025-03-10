1e6b97dd22905282d28a1a247a82a3fb4650500e
PUMBA32 <sokolovgames42@gmail.com>
2025-03-10 21:32:31 +0300

    about page was added

--- sitewomen/sitewomen/urls.py
+++ sitewomen/sitewomen/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from women.views import index, categories
+from women.views import index, categories, about
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index),
-    path('categories/', categories)
+    path('categories/', categories),
+    path('about/', about)
 ]
